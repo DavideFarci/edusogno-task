831ee48c92c116ac37f5dd8a9689560edebf0e26
DavideFarci <yank92.df@gmail.com>
2023-10-02 17:30:02 +0200

    add home and register files

---
+++ register.php
@@ -0,0 +1,16 @@
+<form action="login.php" method="post">
+        <h2>LOGIN</h2>
+        <p class="error"></p>
+
+        <label for="name">Nome</label>
+        <input type="text" name="name" placeholder="Inserisci il tuo nome"><br>
+
+        <label for="surname">Cognome</label>
+        <input type="text" name="surname" placeholder="Inserisci il tuo cognome"><br>
+
+        <label for="pass">Password</label>
+        <input type="password" name="pass">
+
+        <button type="submit">Login</button>
+
+    </form>
