8e06da61f1585609dcc2ee92672a54c77c94343d
DavideFarci <yank92.df@gmail.com>
2023-10-03 11:48:57 +0200

    fix register form

--- register.php
+++ register.php
@@ -1,5 +1,17 @@
-<form action="login.php" method="post">
-        <h2>LOGIN</h2>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/styles/style.css">
+    <title>Edusogno</title>
+</head>
+
+<body>
+    <form action="login.php" method="post">
+        <h2>REGISTER</h2>
         <p class="error"></p>
 
         <label for="name">Nome</label>
@@ -8,9 +20,15 @@
         <label for="surname">Cognome</label>
         <input type="text" name="surname" placeholder="Inserisci il tuo cognome"><br>
 
+        <label for="email">Email</label>
+        <input type="email" name="email" placeholder="Inserisci la tua email"><br>
+
         <label for="pass">Password</label>
         <input type="password" name="pass">
 
-        <button type="submit">Login</button>
+        <button type="submit">Register</button>
 
     </form>
+</body>
+
+</html>
