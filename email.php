375b27747c44d008916dd627edb2d3893d7377cd
DavideFarci <yank92.df@gmail.com>
2023-10-05 18:25:59 +0200

    add control for email in database

--- email.php
+++ email.php
@@ -13,38 +13,45 @@ include "database.php";
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
+require 'vendor/autoload.php';
 
 if (isset($_POST['email'])) {
+    // verifico se l'email è presente nel database 
     $email = $_POST["email"];
-    require 'vendor/autoload.php';
-    $mail = new PHPMailer(true);
-    try {
-        $mail->isSMTP();
-        $mail->SMTPAuth   = true;
-        $mail->Host       = 'smtp.mailtrap.io'; // Hostname di Mailtrap
-        $mail->Username   = 'ca63e1fbfe9ca9'; // Nome utente di Mailtrap
-        $mail->Password   = '0015b531a04475'; // Password di Mailtrap
-        $mail->SMTPSecure = 'tls';
-        $mail->Port       = 2525; // Porta SMTP di Mailtrap
-    
-        $mail->setFrom('from@example.com');
-        $mail->addAddress($email);
-        $mail->addReplyTo('edusogno@exaple.com');
-    
-        $mail->isHTML(true);
-        $mail->Subject = "Password Reset";
-        $mail->Body    = <<<END
-
-        Click <a href="http://localhost:8080/EduSogno/edusogno-esercizio/reset_password.php">here</a> to reset to password.
-    
-        END;;
+    $sqlMail = "SELECT * FROM utenti WHERE email='$email'";
+    $result = mysqli_query($conn, $sqlMail);
+    if (mysqli_num_rows($result) === 1) {
+        $mail = new PHPMailer(true);
+        try {
+            $mail->isSMTP();
+            $mail->SMTPAuth   = true;
+            $mail->Host       = 'smtp.mailtrap.io'; // Hostname di Mailtrap
+            $mail->Username   = 'ca63e1fbfe9ca9'; // Nome utente di Mailtrap
+            $mail->Password   = '0015b531a04475'; // Password di Mailtrap
+            $mail->SMTPSecure = 'tls';
+            $mail->Port       = 2525; // Porta SMTP di Mailtrap
         
-        $mail->send();
-        echo 'Il messaggio è stato inviato con successo, controlla la tua mail o inbox.';
+            $mail->setFrom('from@example.com');
+            $mail->addAddress($email);
+            $mail->addReplyTo('edusogno@exaple.com');
         
-    } catch (Exception $e) {
-        echo "Impossibile inviare il messaggio. Errore Mailer: {$mail->ErrorInfo}";
-    }
+            $mail->isHTML(true);
+            $mail->Subject = "Password Reset";
+            $mail->Body    = <<<END
+    
+            Click <a href="http://localhost:8080/EduSogno/edusogno-esercizio/reset_password.php">here</a> to reset to password.
+        
+            END;;
+            
+            $mail->send();
+            echo 'Il messaggio è stato inviato con successo, controlla la tua mail o inbox.';
+            
+        } catch (Exception $e) {
+            echo "Impossibile inviare il messaggio. Errore Mailer: {$mail->ErrorInfo}";
+        }
+    }else {
+        echo 'La mail non è presente nel nostro database';
+    } 
 }?>
 
 
