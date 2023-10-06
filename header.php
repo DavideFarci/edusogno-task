2e1216bf4478d677b870975e56776ea4da3031c3
DavideFarci <yank92.df@gmail.com>
2023-10-06 11:47:37 +0200

    add button home in header, change color h1 dashb.

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
 <header>
-    <img src="assets/img/logo_svg.svg" alt="logo">
+    <a href="home.php"><img src="assets/img/logo_svg.svg" alt="logo"></a>
     <div><a class="dash" href="dashboard.php">Dashboard</a></div>
 </header>
